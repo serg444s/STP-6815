f6ecf9f171e732fc53690b2b27418048aaf29afb
Serhii Savchenko <sergey.savchenko1303@gmail.com>
2024-10-07 01:15:59 +0300

    add users reviews mobile section

--- main.js
+++ main.js
@@ -32,3 +32,26 @@ function navigate(event) {
 
 openModalBtn.addEventListener("click", onOpenModal);
 closeModalBtn.addEventListener("click", onCloseModal);
+
+function screenSize() {
+  if (window.innerWidth >= 1440) {
+    return 3;
+  } else {
+    return 1;
+  }
+  r;
+}
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  slidesPerView: screenSize(),
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+window.addEventListener("resize", screenSize);
